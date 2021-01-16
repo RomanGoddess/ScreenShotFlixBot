d06e2b56a6acd98fc6cc61951df6038ba6e52ef8
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-01-16 17:23:50 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Config:
     DATABASE_URL = os.environ.get('DATABASE_URL')
     AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
     MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
-    MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
+    MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 99999999999))
     TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
     SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 15))
     HOST = os.environ.get('HOST', '')
